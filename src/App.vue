bcdc358365cf6f33622ac9b61b41e22b3291ddb5
Jerry Wang <x.jerry.wang@gmail.com>
2021-10-13 23:22:09 +0800

    chore: fix build

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import { AudioOption } from './components/types'
 import VAudio from './components/VAudio.vue'
 import VVideo from './components/VVideo.vue'
 
+const base = import.meta.env.BASE_URL
+
 const audios: AudioOption[] = [
   {
     name: 'mp3-1',
-    url: '/audio-1.mp3',
-    cover: '/cover.png',
+    url: base + 'audio-1.mp3',
+    cover: base + 'cover.png',
   },
   {
     name: 'mp3-2',
-    url: '/audio-2.mp3',
-    cover: '/cover.png',
+    url: base + 'audio-2.mp3',
+    cover: base + 'cover.png',
   },
   {
     name: 'mp3-3',
-    url: '/audio-3.mp3',
-    cover: '/cover.png',
+    url: base + 'audio-3.mp3',
+    cover: base + 'cover.png',
   },
 ]
 </script>
